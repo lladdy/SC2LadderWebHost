425d5cb3f69446c5a82c39c1aac41b1597d30cd1
lladdy <randallboland@bigpond.com>
2019-03-02 02:54:00 +1030

    Rename dbconf.php for db versioning to use as an example.

---
+++ db/dbconf-example.php
@@ -0,0 +1,6 @@
+<?php
+//DATABASE CONNECTION VARIABLES FOR VERSIONING
+$host = "localhost"; // Host name. Use "db" for docker setups as it should resolve to the database container
+$username = "root"; // Mysql username
+$password = "root"; // Mysql password
+$db_name = "sc2ladder"; // Database name
